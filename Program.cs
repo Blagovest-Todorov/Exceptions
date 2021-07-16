69d5c0a065a4846df6fe9e0997f0333ac9b56cad
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-07-16 12:35:44 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(CalcSqrt(b));
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (FormatException fex)
+            {
+                Console.WriteLine(fex.Message);
+            }
+            catch (Exception  ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+
+        public static double CalcSqrt(int a)
+        {       
+            if (a <= 0)
+            {
+                throw new ArgumentException("Invalid number");
+            }
+
+             double result = Math.Sqrt(a);
+             return result;
+        }
+    }
+}
